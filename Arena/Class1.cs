804b632d48a5291deb388948768421a9dc844840
catandladybug <98188215+catandladybug@users.noreply.github.com>
2022-02-17 19:27:59 -0700

    aaa

---
+++ Arena/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehavior : MonoBehaviour
+{
+
+	public Transform patrolRoute;
+	public List<Transform> locations;
+
+	private int locationIndex = 0;
+
+	private NavMeshAgent agent;
+
+	private void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+
+		InitializePatrolRoute();
+
+		MoveToNextPatrolLocation();
+
+	}
+
+	void InitalizePatrolRoute()
+	{
+		foreach (Transform child in patrolRoute)
+		{
+
+			locations.Add(child);
+
+		}
+	}
+
+	void MoveToNextPatrolLocation();
+	{
+		agent.destination = LocationService[locationsIndex].position;
+	}
+
+
+void OnTriggerEnter(Collider other)
+{
+	if (other.name == "Player")
+	{
+		Debug.Log("Player detected - Attack!");
+	}
+}
+
+void OnTriggerExit(Collider other)
+{
+	if (other.name == "Player")
+	{
+		Debug.Log("Player out of range, resume patrol");
+	}
+}
+}
+   
